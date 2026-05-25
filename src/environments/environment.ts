bae468943374b8356aa9b0061eab3ebcbe259b3a
Lopescamos <lopezmontalvokevin2021@gmail.com>
2026-05-24 20:20:06 -0500

    chore: add environment configuration for development and production

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'http://localhost:3001',
+  platformProviderApiBaseUrl: 'http://localhost:3001',
+  platformProviderUserProfilesEndpointPath: '/api/user-profiles',
+  platformProviderTeacherNotesEndpointPath: '/api/teacher-notes',
+  platformProviderTherapySessionsEndpointPath: '/api/therapy-sessions',
+  platformProviderSessionChecklistEndpointPath: '/api/session-checklist',
+  platformProviderEmergenciesEndpointPath: '/api/emergencies',
+  platformProviderEventosEndpointPath: '/api/eventos',
+  platformProviderRecordatoriosEndpointPath: '/api/recordatorios',
+};
