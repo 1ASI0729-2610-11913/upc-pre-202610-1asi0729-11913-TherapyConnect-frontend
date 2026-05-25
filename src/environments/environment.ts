0e1b7c46c4fcd8a15df00f39b1edda8b55d92d4d
GZ-99 <gabrielvilchezvite@gmail.com>
2026-05-24 23:18:05 -0500

    chore: initial commit.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,4 +9,8 @@ export const environment = {
   platformProviderEmergenciesEndpointPath: '/api/emergencies',
   platformProviderEventosEndpointPath: '/api/eventos',
   platformProviderRecordatoriosEndpointPath: '/api/recordatorios',
+
+  platformProviderProductsEndpointPath: 'api/products',
+  platformProviderDependentsEndpointPath: 'api/dependents',
+  platformProviderCatalogsEndpointPath: 'api/product-catalogs',
 };
