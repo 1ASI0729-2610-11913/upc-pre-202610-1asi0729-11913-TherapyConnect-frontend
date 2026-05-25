c5e5e41ab37cbcfbacaa2da5cffe5ca4f12f6014
GZ-99 <gabrielvilchezvite@gmail.com>
2026-05-24 23:42:16 -0500

    feat(api): Add courses-api-endpoint.ts, registrations-api-endpoint.ts, evaluations-api-endpoint.ts, observations-api-endpoint.ts and course-and-learning-managment-api.ts

---
+++ src/app/marketplace-and-recommendations/infrastructure/courses-api-endpoint.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { BaseApiEndpoint } from '../../shared/infrastructure/base-api-endpoint';
+import { Course } from '../domain/model/course.entity';
+import { CourseAssembler } from './course-assembler';
+import { CourseResource, CourseResponse } from './course-response';
+
+export class CoursesApiEndpoint extends BaseApiEndpoint<Course, CourseResource, CourseResponse, CourseAssembler> {
+  constructor(http: HttpClient) {
+    super(
+      http,
+      `${environment.platformProviderApiBaseUrl}${environment.platformProviderCoursesEndpointPath}`,
+      new CourseAssembler(),
+    );
+  }
+}
